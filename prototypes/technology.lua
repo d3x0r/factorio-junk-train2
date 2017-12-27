33ef0ff1e1e4b4e9de97759328323a3936341aee
d3x0r <d3x0r@no-reply.github.com>
2017-12-27 01:38:04 -0800

    Initial commit

---
+++ prototypes/technology.lua
@@ -0,0 +1,65 @@
+data:extend({
+    {
+        type = "technology",
+        name = "JunkTrain_tech",
+        icon = "__JunkTrain2__/graphics/tech-icon.png",
+        icon_size = 128,
+        effects = 
+        {
+   		{
+				type = "unlock-recipe",
+				recipe = "scrap-rail"
+		},
+            {
+                type = "unlock-recipe",
+                recipe = "JunkTrain"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "ScrapTrailer"
+            }
+        },    
+        prerequisites = {"automation"},
+        unit =
+        {
+          count = 20,
+          ingredients =
+          {
+            {"science-pack-1", 1},
+          },
+          time = 20
+        },
+        order = "c-g-b-a"
+    },
+   {
+    type = "technology",
+    name = "automated-scrap-rail-transportation",
+    icon = "__base__/graphics/technology/automated-rail-transportation.png",
+    effects =
+    {
+	    {
+                type = "unlock-recipe",
+                recipe = "train-stop-scrap"
+            },
+	    {
+                type = "unlock-recipe",
+                recipe = "rail-signal-scrap"
+            },
+	    {
+                type = "unlock-recipe",
+                recipe = "rail-chain-signal-scrap"
+            }
+    },
+    prerequisites = {"JunkTrain_tech","optics"},
+    unit =
+    {
+      count = 50,
+      ingredients =
+      {
+        {"science-pack-1", 1},
+      },
+      time = 30
+    },
+    order = "c-g-b",
+  }
+})
